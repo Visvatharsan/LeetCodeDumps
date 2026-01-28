4cc1233b84df768c7943c90f57096832c6587469
Visvatharsan <149995659+Visvatharsan@users.noreply.github.com>
2026-01-28 13:27:29 +0530

    Clean up commented code in searchRange function
    Removed commented-out code for first and last position search functions.

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,5 @@
 class Solution {
 public:
-    // int first(vector<int>& nums, int target){
-    //     int start=0, end=nums.size()-1;
-    //     int mid,first;
-
-    //     while(start<=end){
-    //         mid=(start+end)/2;
-    //         if(nums[mid]>=target){
-    //             if(nums[mid] == target)
-    //                 first=mid;
-    //             start=mid+1;
-    //         }
-    //         else{
-    //             start=mid+1;
-    //         }
-    //     }
-    //     return -1;
-    // }
-
-    // int last(vector<int>& nums, int target){
-    //     int start=0, end=nums.size()-1;
-    //     int mid,last=-1;
-
-    //     while(start<=end){
-    //         mid=(start+end)/2;
-    //         if(nums[mid]>=target){
-    //             if(nums[mid] == target)
-    //             first=mid;
-    //             end=mid-1;
-    //         }
-    //         else{
-    //             start=mid+1;
-    //         }
-    //     }
-    //     return -1;
-    // }
-
-
     vector<int> searchRange(vector<int>& nums, int target) {
         int startingPosition = -1, endingPosition = -1;
         int n = nums.size();
