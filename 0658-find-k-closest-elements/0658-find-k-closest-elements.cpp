7eafc929f3b5e73d92145bf687bf55c8672b9022
Visvatharsan <149995659+Visvatharsan@users.noreply.github.com>
2026-02-21 16:10:18 +0530

    Time: 35 ms (9.05%), Space: 38.6 MB (26.94%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -3,19 +3,16 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n=arr.size();
         priority_queue<pair<int,int>> pq;
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0; i<n; i++){
             pq.push({abs(arr[i]-x),arr[i]});
-            if(pq.size()>k)
-            {
+            if(pq.size() > k){
                 pq.pop();
             }
         }
+
         vector<int> ans;
-        while(!pq.empty())
-        {
-            auto[a,b]=pq.top();
-            ans.push_back(b);
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
             pq.pop();
         }
         sort(ans.begin(),ans.end());
